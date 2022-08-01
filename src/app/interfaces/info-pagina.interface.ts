bc56c73b7bda1daed4cb3d4832ebbfd08348f064
Nahuel Gaston Valdez Peralta <nvaldezperalta@gmail.com>
2022-08-01 16:43:48 -0300

    Fin sección 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina {
+  title?: string;
+  email?: string;
+  short_name?: string;
+  autor_page?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  job_group?: any[];
+}
